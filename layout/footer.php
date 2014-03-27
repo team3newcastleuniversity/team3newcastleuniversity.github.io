3a116b74856b8ba48e27e64977049a2c4dda074e
Soroush Atarod <soroush89@live.com>
2014-03-27 17:25:25 +0000

    e

---
+++ layout/footer.php
@@ -0,0 +1,22 @@
+ <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 ">
+                    <h3>Newsletter</h3>
+                    <p>Enter your email, and we will keep updated</p>
+                    <div class="form-group">
+                        <input type="text"  class="form-control" placeholder="your email" />
+                    </div>
+                    <button type="submit" class="btn btn-success">Submit</button>
+                </div>
+                <div class="col-lg-6">
+                    <h3>Sponsors </h3>
+                    <div class="pull-left">
+                        <ul>
+                            <li><a href="http://research.ncl.ac.uk/game/" target="_blank" > <img src="images/lightlogo.png"/></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
---
+++ layout/nav.php
@@ -0,0 +1,13 @@
+<nav id="main-nav">
+    <ul id="nav">
+        <li>
+            <a href="<?php echo $baseurl; ?>image.php">Gallery</a>
+        </li>
+        <li >
+            <a href="<?php echo $baseurl; ?>media.php">Media</a>
+        </li>
+        <li>
+               <a href="<?php echo $baseurl; ?>blog.php">Blog</a>
+        </li>
+    </ul>
+</nav>
